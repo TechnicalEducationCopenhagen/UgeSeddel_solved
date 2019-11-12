4d2996c23bc13ad926de4e65e9d45ca729a32143
Søren Magnusson <soren@magnussons.dk>
2019-11-12 23:58:26 +0100

    start

---
+++ UgeSeddel2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace UgeSeddel2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Inddata
+
+            Console.Write("Navn     :");
+            string navn = Console.ReadLine();
+
+            Console.Write("Timeløn  :");
+            double timeløn = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Timer    :");
+            double antalTimer = Convert.ToDouble(Console.ReadLine());
+
+            // Beregn
+
+            double løn = 0;
+            double overTimer = 0;
+            double normalLøn = 0;
+            double overTidsLøn = 0;
+
+            if (antalTimer <= 37.5)
+            {
+                løn = antalTimer * timeløn;
+            }
+            else
+            {
+                overTimer = antalTimer - 37.5;
+                normalLøn = 37.5 * timeløn;
+                overTidsLøn = overTimer * timeløn * 1.5;
+                løn = normalLøn + overTidsLøn;
+            }
+
+            // Udskriv
+            // Console.WriteLine(løn);
+
+            // udskriv header
+            Console.WriteLine("LØNSEDDEL {0, 69}", "FOR " + navn);
+            Console.WriteLine("===============================================================================");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            if (overTimer == 0)
+            {
+                // normal timer
+                Console.WriteLine("{0, 6:0.00} timer á {1, 6:0.00}{2, 58:0.00}", antalTimer, timeløn, løn);
+                Console.WriteLine("                              --------");
+            }
+            else
+            {
+                // over timer
+                Console.WriteLine("{0} timer i alt", antalTimer);
+                Console.WriteLine("    heraf");
+                Console.WriteLine("      {0, 6:0.00} timer á {1, 6:0.00}{2, 52:0.00}", 37.5, timeløn, normalLøn);
+                Console.WriteLine("      {0, 6:0.00} timer á {1, 6:0.00}{2, 52:0.00}", overTimer, timeløn * 1.5, overTidsLøn);
+                Console.WriteLine("                              --------");
+            }
+            // ialt 
+            Console.WriteLine("                              I Alt: Kr. {0}", løn);
+            Console.WriteLine("                              ==================");
+
+            // bund
+            Console.WriteLine("------------------------------------------------------");
+        }
+    }
+}
---
+++ automatiskTest/UnitTest1.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+
+using System.IO;
+using System.Text.RegularExpressions;
+
+using UgeSeddel2;
+
+namespace automatiskTest
+{
+    public class UnitTest1
+    {
+        StringWriter output;
+        StringReader input;
+
+        String pattern;
+        Regex regex;
+
+        public UnitTest1()
+        {
+            output = new StringWriter();
+            Console.SetOut(output);
+
+            input = new StringReader("");
+            Console.SetIn(input);
+        }
+
+        [Fact]
+        public void Dialog1()
+        {
+            Program.Main(new String[] { });
+
+            string resultat = this.output.ToString();
+
+            Assert.Contains(
+                "Navn     :", resultat
+            );
+
+            Assert.Contains(
+                "Timeløn  :", resultat
+            );
+
+            Assert.Contains(
+                "Timer    :", resultat
+            );
+        }
+
+        [Fact]
+        public void Dialog2()
+        {
+            input = new StringReader("Søren\n200\n40\n");
+            Console.SetIn(input);
+
+            Program.Main(new String[] { });
+
+            string resultat = this.output.ToString();
+
+            Assert.Contains(
+                "Navn     :", resultat
+            );
+
+            Assert.Contains(
+                "Timeløn  :", resultat
+            );
+
+            Assert.Contains(
+                "Timer    :", resultat
+            );
+
+            Assert.Contains(
+                "FOR Søren", resultat
+            );
+        }
+    }
+}
